f0517127f7893b5176358d3381d67fe58ad3cbff
Anderson Domingos <andomingos87@gmail.com>
2024-02-26 12:58:55 -0300

    alteração na chamada api de fetch para XMLHttpRequest

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,41 +2,30 @@ document.getElementById('meuFormulario').addEventListener('submit', function (e)
     e.preventDefault();
 
     var data = {
-        nomecompleto: document.getElementById('nomecompleto').value, // Garanta que o ID está correto
+        nomecompleto: document.getElementById('nomecompleto').value,
         email: document.getElementById('email').value,
         whatsapp: document.getElementById('whatsapp').value,
         cidade: document.getElementById('cidade').value,
-        tipolead: document.getElementById('tipolead').value, // Garanta que o ID está correto
+        tipolead: document.getElementById('tipolead').value,
         estado: document.getElementById('estado').value,
-        segmento: document.getElementById('segmento').value, // Garanta que o ID está correto
-        produtointeresse: document.getElementById('produtointeresse').value // Garanta que o ID está correto
+        segmento: document.getElementById('segmento').value,
+        produtointeresse: document.getElementById('produtointeresse').value
     };
 
-    // Url da API em produção
-    fetch('https://tecnoportasleads.com.br/api/1.1/wf/novo-lead', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-            // Se necessário, adicione o cabeçalho de autorização aqui
-        },
-        body: JSON.stringify(data)
-    })
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'https://tecnoportasleads.com.br/api/1.1/wf/novo-lead', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('Success:', xhr.responseText);
+                window.location.href = "obrigado.html";
+            } else {
+                console.error('Error:', xhr.statusText);
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Success:', data);
-            // Tratar a resposta de sucesso aqui
-            this.reset()
-            window.location.href = "obrigado.html"
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            // Tratar o erro aqui
-        });
+        }
+    };
 
+    xhr.send(JSON.stringify(data));
 });
